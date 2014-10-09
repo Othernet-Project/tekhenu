6d9328c6cf7d16a9e7c679fd58be26ba1f068e89
Branko Vukelic <branko@brankovukelic.com>
2014-10-09 18:51:45 +0200

    Fixed mail sending issues

--- tekhenu/lib/notify.py
+++ tekhenu/lib/notify.py
@@ -21,27 +21,25 @@ from google.appengine.api import mail
 
 def send_notification(subject, body):
     """ Send notification email to configured admin emails """
-    admins = [s.strip()
-              for s in request.app.config['notify.admins'].split(',')]
+    admin = request.app.config['notify.admin']
     sender = request.app.config['notify.sender']
     prefix = request.app.config.get('notify.subject_prefix')
     if prefix:
         subject = prefix + ' ' + subject
-    for admin in admins:
-        mail.send_mail(sender, admin, subject, body)
+    mail.send_mail(sender, admin, subject, body)
 
 
 def format_dict(d):
     """ Format dictionary as human-readable text """
     s = '\n'
     for k in d.keys():
-        s += '    %s: '
+        s += '    %s: ' % k
         try:
             v = d.getall(k)
             if len(v) > 1:
                 s += ', '.join(v)
             elif len(v) == 1:
-                s += v[0] or '(empty or None)'
+                s += v[0] and ('%r' % v[0]) or '(empty or None)'
             else:
                 s += '(empty or None)'
         except AttributeError:
@@ -50,7 +48,7 @@ def format_dict(d):
                 if isinstance(v, (list, tuple)):
                     s += ', '.join(v)
                 else:
-                    s += v or 'None'
+                    s += v and ('%r' % v) or 'None'
             except KeyError:
                 s += 'KeyError'
         except KeyError:
@@ -64,12 +62,13 @@ def handle_error(exc, extras={}):
     msgs = []
     msgs.append("TIMESTAMP: %s" % datetime.datetime.utcnow())
     msgs.append("ERROR: %s" % exc.exception)
-    msgs.append("\n%s\n" % exc.traceback)
-    msgs.append("PATH: %s %s" % (request.method, request.path))
+    msgs.append("PATH: %s %s" % (request.method, request.fullpath))
+    msgs.append("PARAMS: %s" % format_dict(request.params))
     msgs.append("HEADERS: %s" % format_dict(request.headers))
     msgs.append("COOKIES: %s" % format_dict(request.cookies))
     msgs.append("IS XHR: %s" % 'yes' if request.is_xhr else 'no')
     msgs.append("IP: %s" % request.remote_addr)
+    msgs.append("\n%s\n" % exc.traceback)
     if extras:
         msgs.append("EXTRA INFORMATION: %s" % format_dict(extras))
     subject = "'%s' at %s" % (exc.exception, request.path)
